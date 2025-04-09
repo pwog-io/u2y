1f3b12c510c42f593dce00fba989389737ef1ef8
Huberson D. <22603770+hubersond@users.noreply.github.com>
2025-04-08 23:29:02 -0400

    [package/react]Adds linting

---
+++ packages/react/eslint.config.mjs
@@ -0,0 +1,36 @@
+import 'eslint-plugin-only-warn'
+import { defineConfig } from 'eslint/config'
+import { base, jsxA11y, react, tsStrictTypeChecked } from '@pwog/eslint-config'
+
+export default defineConfig([
+    base({ name: 'base', files: ['**/*.{js,jsx,mjs,ts,tsx}'] }, true),
+    tsStrictTypeChecked({
+        name: 'ts-tsx',
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.build.json',
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    }),
+    react({ name: 'tsx', files: ['**/*.tsx'] }),
+    jsxA11y({ name: 'tsx', files: ['**/*.tsx'] }),
+    {
+        name: '[tools-lint]',
+        files: [
+            './eslint.config.mjs',
+            './vite.config.ts',
+        ],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.node.json',
+            },
+        },
+    },
+    // global ignore: https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+    {
+        name: '[global-ignores]',
+        ignores: ['dist/*'],
+    },
+])

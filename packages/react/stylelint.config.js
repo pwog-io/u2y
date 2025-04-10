d262ae6f3aa1014b197e510cc8723dfaf31e0404
Huberson D. <22603770+hubersond@users.noreply.github.com>
2025-04-09 21:51:05 -0400

    chore: Add Stylelint configuration and update dependencies in React package
    - Introduced a new Stylelint configuration file for CSS linting.
    - Updated package.json to include Stylelint and its standard configuration as devDependencies.
    - Added a linting script for CSS files to the npm scripts section.

---
+++ packages/react/stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+    defaultSeverity: 'warning',
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'color-hex-length': 'long',
+    },
+}
